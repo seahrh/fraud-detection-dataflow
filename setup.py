a6f2f50eae557c742ce65d42250365d7efb76b0e
Ruhong <seahrh@gmail.com>
2020-04-26 11:45:06 +0800

    add black lint

--- setup.py
+++ setup.py
@@ -1,25 +1,18 @@
 from setuptools import setup, find_packages
 
-__version__ = '1.0'
+__version__ = "1.0"
 setup(
-    name='acme.fraudcop',
+    name="acme.fraudcop",
     version=__version__,
-    python_requires='>=3.7.0',
-    install_requires=[
-        'apache-beam[gcp]==2.20.*',
-    ],
-    extras_require={
-        'tests': [
-            'mypy>=0.770',
-            'pytest>=5.4.1',
-        ]
-    },
-    packages=find_packages('src', exclude=['*.tests', '*.tests.*', 'tests.*', 'tests']),
-    package_dir={'': 'src'},
+    python_requires="==3.7.*",
+    install_requires=["apache-beam[gcp]==2.20.*"],
+    extras_require={"tests": ["black==19.*", "mypy>=0.770", "pytest>=5.4.1"]},
+    packages=find_packages("src", exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+    package_dir={"": "src"},
     include_package_data=True,
-    description='acme fraudcop',
-    license='All rights reserved',
-    author='seahrh',
-    author_email='seahrh@gmail.com',
-    url='https://github.com/seahrh/acme.fraudcop'
+    description="acme fraudcop",
+    license="All rights reserved",
+    author="seahrh",
+    author_email="seahrh@gmail.com",
+    url="https://github.com/seahrh/acme.fraudcop",
 )
