a092ff75a4efb5e60de64e33f2e3b32c07f5babc
Ruhong <seahrh@gmail.com>
2020-04-27 03:14:08 +0800

    create Imput PTransform

--- src/acme/fraudcop/etl/__init__.py
+++ src/acme/fraudcop/etl/__init__.py
@@ -3,30 +3,38 @@ from typing import NamedTuple, Dict, Any, Iterable
 import apache_beam as beam
 
 
-class Rule(NamedTuple):
-    key: str
-    imputed_value: Any
-    apply_on_nulls: bool = True
-    apply_on_empty_strings: bool = False
-    apply_on_zeroes: bool = False
-    apply_on_negative_numbers: bool = False
+class Imput(beam.PTransform):
+    class Rule(NamedTuple):
+        key: str
+        imputed_value: Any
+        apply_on_nulls: bool = True
+        apply_on_empty_strings: bool = False
+        apply_on_zeroes: bool = False
+        apply_on_negative_numbers: bool = False
 
+    @staticmethod
+    def transform(element: Dict[str, Any], rules: Iterable[Rule]) -> Dict[str, Any]:
+        res = dict(element)
+        for rule in rules:
+            if rule.key not in res:
+                continue
+            v = res[rule.key]
+            if rule.apply_on_nulls and v is None:
+                res[rule.key] = rule.imputed_value
+            if rule.apply_on_empty_strings and v == "":
+                res[rule.key] = rule.imputed_value
+            if rule.apply_on_zeroes and v == 0:
+                res[rule.key] = rule.imputed_value
+            if rule.apply_on_negative_numbers and v < 0:
+                res[rule.key] = rule.imputed_value
+        return res
 
-def imput(element: Dict[str, Any], rules: Iterable[Rule]) -> Dict[str, Any]:
-    res = dict(element)
-    for rule in rules:
-        if rule.key not in res:
-            continue
-        v = res[rule.key]
-        if rule.apply_on_nulls and v is None:
-            res[rule.key] = rule.imputed_value
-        if rule.apply_on_empty_strings and v == "":
-            res[rule.key] = rule.imputed_value
-        if rule.apply_on_zeroes and v == 0:
-            res[rule.key] = rule.imputed_value
-        if rule.apply_on_negative_numbers and v < 0:
-            res[rule.key] = rule.imputed_value
-    return res
+    def __init__(self, rules: Iterable[Rule]):
+        super().__init__()
+        self.rules = rules
+
+    def expand(self, input_or_inputs):
+        return input_or_inputs | beam.Map(Imput.transform, rules=self.rules)
 
 
 class StandardDeviationCombineFn(beam.CombineFn):
--- tests/test_etl.py
+++ tests/test_etl.py
@@ -2,30 +2,34 @@ from acme.fraudcop.etl import *
 
 
 class TestImput:
+    # noinspection PyCallByClass
     def test_imput_on_nulls(self):
         k = "mykey"
         v = "imputed"
         inp = {k: None}
-        rules = [Rule(key=k, imputed_value=v, apply_on_nulls=True)]
-        assert imput(inp, rules) == {k: v}
+        rules = [Imput.Rule(key=k, imputed_value=v, apply_on_nulls=True)]
+        assert Imput.transform(inp, rules) == {k: v}
 
+    # noinspection PyCallByClass
     def test_imput_on_empty_strings(self):
         k = "mykey"
         v = "imputed"
         inp = {k: ""}
-        rules = [Rule(key=k, imputed_value=v, apply_on_empty_strings=True)]
-        assert imput(inp, rules) == {k: v}
+        rules = [Imput.Rule(key=k, imputed_value=v, apply_on_empty_strings=True)]
+        assert Imput.transform(inp, rules) == {k: v}
 
+    # noinspection PyCallByClass
     def test_imput_on_zeroes(self):
         k = "mykey"
         v = -1
         inp = {k: 0}
-        rules = [Rule(key=k, imputed_value=v, apply_on_zeroes=True)]
-        assert imput(inp, rules) == {k: v}
+        rules = [Imput.Rule(key=k, imputed_value=v, apply_on_zeroes=True)]
+        assert Imput.transform(inp, rules) == {k: v}
 
+    # noinspection PyCallByClass
     def test_imput_on_negative_numbers(self):
         k = "mykey"
         v = 0
         inp = {k: -1}
-        rules = [Rule(key=k, imputed_value=v, apply_on_negative_numbers=True)]
-        assert imput(inp, rules) == {k: v}
+        rules = [Imput.Rule(key=k, imputed_value=v, apply_on_negative_numbers=True)]
+        assert Imput.transform(inp, rules) == {k: v}
